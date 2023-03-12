3d258809eb5c0527c37cd0f5ed3909660141701c
EkaterinaSterhova <126870774+EkaterinaSterhova@users.noreply.github.com>
2023-03-12 19:25:44 +0600

    Examples005_IF pridject

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //любимчик
+        Console.Write("Введите имя пользовалеля: ");
+        string username = Console.ReadLine();
+
+        if (username.To.Lower() == "маша") ;
+    {
+            Console.WriteLine("Ура, это же Маша!");
+    }
+else
+        {
+            Console.WriteLine("Привет, ");
+            Console.WriteLine(username);
+        }
+    }
+}
